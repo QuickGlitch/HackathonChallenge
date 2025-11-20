7bf2bb23a5e9f593c340762897fbbf48cd1f74e3
adb <antondebode@gmail.com>
2025-11-20 17:05:29 +0100

    added ctf flag

--- apps/backend/src/routes/products.js
+++ apps/backend/src/routes/products.js
@@ -48,7 +48,7 @@ const upload = multer({
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
     } else {
-      cb(new Error("Only image files are allowed!"), false);
+      cb(new Error("Oh mime! Only image files are allowed 🤔 "), false);
     }
   },
 });
