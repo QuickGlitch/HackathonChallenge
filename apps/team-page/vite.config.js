4ca822e5eff9e92b9770e5130329f25b03f9fb4c
adb <antondebode@gmail.com>
2025-12-03 21:17:54 +0100

    fixed local/dev routing of team-page

--- apps/team-page/vite.config.js
+++ apps/team-page/vite.config.js
@@ -7,6 +7,11 @@ export default defineConfig({
   server: {
     port: 5175,
     host: true,
-    proxy: {},
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        changeOrigin: true,
+      },
+    },
   },
 });
